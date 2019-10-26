dfc1d8e7c91b2cdf6ea8ef87cf6f2d1ea9a9b32d
podit <tomody@outlook.com>
2019-10-26 23:55:55 +0100

    it's alive

--- servon/snek.py
+++ servon/snek.py
@@ -1,30 +1,60 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import json
+import pokepy
 
 class Baddel:
-    def __init__(self):
-        self.hoho = 'ehehe'
+    one = {"tag":False}
+    two = {"tag":False}
+    ready = False
+    
+    def join(self, pkmnId, name, moveIds):
+        if not self.one["tag"]:
+            self.one["tag"] = True
+            self.one["pkmn"] = pkmnId
+            self.one["name"] = name
+            self.one["moves"] = moveIds
+            return 'one'
+        elif not self.two["tag"]:
+            self.two["tag"] = True
+            self.two["pkmn"] = pkmnId
+            self.two["name"] = name
+            self.two["moves"] = moveIds
+            return 'two'
+        else:
+            return 'piss off'
 
-    def rekt(self):
-        return 'ownd'
-    def ownd(self):
-        return self.hoho
+    def start(self):
+        if self.one["tag"] and self.two["tag"]:
+            return 'woo'
+
+    def sho(self):
+        print(self.one, self.two)
+
+
+app = Flask(__name__)
+
+b = Baddel()
+
 
 def base():
     return 'Hey'
 
 def hi():
+    b.sho()
     return 'Hi There...'
 
 def plus(num, pls):
     ans = num + pls
     return str(ans)
 
-app = Flask(__name__)
 
-b = Baddel()
+@app.route('/joinson', methods = ['POST'])
+def joinson():
+    con = request.json
+    
+    res = b.join(con['pkmn'], con['name'], con['moves'])
 
-app.add_url_rule('/ownd', 'Ownd', (lambda: b.ownd()))
-app.add_url_rule('/rekt', 'Rekd', (lambda: b.rekt()))
+    return res
 
 app.add_url_rule('/', 'Home', (lambda: base()))
 app.add_url_rule('/hi', 'Hi', (lambda: hi()))
@@ -32,6 +62,6 @@ app.add_url_rule('/plus/<int:num>/<int:pls>', 'Add',
         (lambda num, pls: plus(num, pls)))
 
 if __name__ == "__main__":
-    app.run(host='192.168.69.1', port=42069)
+    app.run(host='localhost', port=42069)
 
 
